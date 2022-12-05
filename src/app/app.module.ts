1f0692702a3f6cad718ebb00c6bb1a6940ce7c8a
Jean Pool Oliver Yantas Inocente <jean.yantas@gmail.com>
2022-12-04 20:02:33 -0500

    configuracion del CRUD en el componente home con formulario template, se genero un modelo por CLI en lugar de una interfaz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// IMPORTAR FORMULARIO TEMPLATE
-// import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -20,7 +17,6 @@ import { Modulo4Module } from './modulo4/modulo4.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    // FormsModule, // FORMULARIO TEMPLATE
     Modulo1Module,
     Modulo2Module,
     Modulo3Module,
--- src/app/modulo2/home/home.component.ts
+++ src/app/modulo2/home/home.component.ts
@@ -1,17 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-// Se crea la interface para el objeto resenia
-interface objetoResenias {
-  dniUsuario: string;
-  nombreUsuario: string;
-  apellidosUsuario: string;
-  correoUsuario: string;
-  genero: string;
-  nombreLibro: string;
-  autorLibro: string;
-  puntuacionLibro: number;
-  comentario: string;
-};
+import { Resenia } from './models/resenia'; // Importar el modelo Resenia que se creó antes (Dentro del componente)
 
 @Component({
   selector: 'app-home',
@@ -19,79 +8,34 @@ interface objetoResenias {
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  // Se crea un array listaResenias para guardar todos los objetos que se crean en el formulario del HTML.
-  listaResenias:any[] = [];
+  // Se crea un array reseniaArray para guardar todos los objetos que se crean en el formulario del HTML.
+  reseniaArray: Resenia[] = [];
 
-  // selectedResenia: objetoResenias = {
-  //   dniUsuario: "",
-  //   nombreUsuario: "",
-  //   apellidosUsuario: "",
-  //   correoUsuario: "",
-  //   genero: "",
-  //   nombreLibro: "",
-  //   autorLibro: "",
-  //   puntuacionLibro: 0,
-  //   comentario: ""
-  // };
+  // Crear una variable selectedResenia de tipo clase Resenia como un objeto vacío
+  selectedResenia: Resenia = new Resenia();
 
   // Método DELETE
-  eliminar(resenia:any){
-    for (let i = 0; i < this.listaResenias.length; i++){
-      if (resenia.dniUsuario == this.listaResenias[i].dniUsuario){
-        this.listaResenias.splice(this.listaResenias.indexOf(this.listaResenias[i]), 1);
+  eliminar(resenia: Resenia){
+    for (let i = 0; i < this.reseniaArray.length; i++){
+      if (resenia.id == this.reseniaArray[i].id){
+        this.reseniaArray.splice(this.reseniaArray.indexOf(this.reseniaArray[i]), 1);
       }
     }
+  }
+
+  addOrEdit(){
+    // Crear Reseña
+    if(this.selectedResenia.id === 0){ // Si el id de la reseña es 0, según la clase creada y su valor iniciado en 0 significa que no hemos seleccionado ninguna reseña
+      this.selectedResenia.id = this.reseniaArray.length + 1; // Asignar un valor al id sumandole 1 a la longitud del array (tabla)
+      this.reseniaArray.push(this.selectedResenia); // Agregar el objeto (datos del formulario) al array (tabla)
+    }
+    // Editar Reseña
+    this.selectedResenia = new Resenia(); // Limpiar el formulario
   }
 
   // Método EDIT
-  editar(resenia: objetoResenias){
-    for (let j = 0; j < this.listaResenias.length; j++){
-      let reseniaEdit:any;
-      if (resenia.dniUsuario == this.listaResenias[j].dniUsuario){
-        reseniaEdit = this.listaResenias[j];
-        // console.log(typeof(reseniaEdit));
-        console.log(reseniaEdit);
-        // console.log(reseniaEdit['dniUsuario']);
-        // console.log(reseniaEdit.nombreUsuario);
-        // this.selectedResenia.dniUsuario = this.listaResenias;
-        // this.selectedResenia = resenia;
-        // console.log(this.selectedResenia);
-      }
-    }
-  }
-
-  // Método CREATE
-  enviar(dniUsuarioInput:HTMLInputElement, nombreUsuarioInput:HTMLInputElement,apellidosUsuarioInput:HTMLInputElement, correoUsuarioInput:HTMLInputElement, generoInput:HTMLSelectElement, nombreLibroInput:HTMLInputElement, autorLibroInput:HTMLInputElement, puntuacionLibroInput:HTMLInputElement, comentarioInput:HTMLTextAreaElement){
-    
-    // Validar que los campos no se encuentren vacíos
-    if(dniUsuarioInput.value == '' || nombreUsuarioInput.value == '' || apellidosUsuarioInput.value == '' || correoUsuarioInput.value == '' || generoInput.value == '' || nombreLibroInput.value == '' || autorLibroInput.value == '' || parseInt(puntuacionLibroInput.value) < 1 || comentarioInput.value == ''){
-      alert('¡Completar Todos Los Campos!');
-    }
-    else if (generoInput.value == 'Seleccionar...'){
-      alert('¡El Campo Género es inválido!');
-    }
-    else if (parseInt(puntuacionLibroInput.value) < 1 || parseInt(puntuacionLibroInput.value) > 10){
-      alert('¡El Campo Puntuación esta Fuera del Rango!');
-    }
-    // Capturar los datos del formulario en el objeto resenia
-    else{
-      const resenia: objetoResenias = {dniUsuario: dniUsuarioInput.value, nombreUsuario: nombreUsuarioInput.value, apellidosUsuario: apellidosUsuarioInput.value, correoUsuario: correoUsuarioInput.value, genero: generoInput.value, nombreLibro: nombreLibroInput.value, autorLibro: autorLibroInput.value, puntuacionLibro: parseInt(puntuacionLibroInput.value), comentario: comentarioInput.value};
-
-      // Método READ: Agregar el objeto resenia al array listaResenias
-      this.listaResenias.push(resenia);
-
-      // Limpiar el formulario después del SUBMIT
-      dniUsuarioInput.value = '';
-      nombreUsuarioInput.value = '';
-      apellidosUsuarioInput.value = '';
-      correoUsuarioInput.value = 'jean.yantas@gmail.com';
-      generoInput.value = '';
-      nombreLibroInput.value = '';
-      autorLibroInput.value = '';
-      puntuacionLibroInput.value = '';
-      comentarioInput.value = '';
-    }
-    return false;
+  openForEdit(resenia: Resenia){
+    this.selectedResenia = resenia;
   }
 
   constructor() { }
